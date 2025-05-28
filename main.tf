cfecda0f90ba3065d7d334173b5af24c349652d9
mayur <msmule777@test.com>
2025-05-28 12:55:13 +0530

    s3bucket

--- main.tf
+++ main.tf
@@ -7,3 +7,12 @@ resource "aws_vpc" "gitbase_vpc" {
   }
   
 }
+
+resource "aws_s3_bucket" "gitbase_bucket" {
+  bucket = "gitbase-bucket-12345678"
+  tags = {
+    Name        = "gitbase-bucket"
+    Environment = "gitbase"
+    example     = "gitbase-bucket"
+  }
+}
